194cdcff88d45e02026864c558620a2b68bae179
ahmedhakeem22 <58742511+ahmedhakeem22@users.noreply.github.com>
2024-11-19 16:11:09 +0300

    "Updated header.php to change CSS file paths from '../includes/css/' to '../css/'"

--- doctor/includes/templats/header.php
+++ doctor/includes/templats/header.php
@@ -5,10 +5,10 @@
             <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
             <meta name="viewport" content="width=device-width , initial-scale=1, shrink-to-fit=no">
             <link rel="icon" href="icon1.png" type="image/png" sizes="16x16">
-            <link rel="stylesheet" type="text/css" href="../includes/css/style.css">
+            <link rel="stylesheet" type="text/css" href="../css/style.css">
 
-	 <link rel="stylesheet" href="../includes/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../includes/css/font-awesome.min.css">
+	 <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/font-awesome.min.css">
             <title> Home </title>
             <script src=""></script>
         </head>
