c24014650c195e8a19a368979552dd125e240169
ahmedhakeem22 <58742511+ahmedhakeem22@users.noreply.github.com>
2024-12-25 15:58:08 +0300

    add footer.php

---
+++ doctor/includes/templates/footer.php
@@ -0,0 +1,6 @@
+<!-- includes/templates/footer.php -->
+    <!-- ربط Bootstrap JS و Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- إضافة أي سكريبتات إضافية هنا -->
+</body>
+</html>
