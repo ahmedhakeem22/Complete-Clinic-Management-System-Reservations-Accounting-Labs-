d2d033122e642adce90eee5a9256e841603f085b
ahmedhakeem22 <58742511+ahmedhakeem22@users.noreply.github.com>
2024-12-25 15:55:54 +0300

    Commit for reception/Bloodtest.php

--- reception/Bloodtest.php
+++ reception/Bloodtest.php
@@ -1,271 +1,135 @@
-<?php include 'includes/templates/header.php';
-       include 'includes/templates/navbar.php';
-       
-	?>
+<?php
+// Bloodtest.php
 
+// 1) استدعاء ملف الاتصال بقاعدة البيانات
+include '../includes/db.php';
 
-      <main>
-
-      
-           
+// 2) استدعاء ملفات الواجهة (هيدر/نافبار)
+include 'includes/templates/header.php';
+include 'includes/templates/navbar.php';
+?>
+<main>
     <img src="includes/images/Bloodtest.jpg" alt="image" width="100%" height="auto">
-    <div class="boxlab" style="top:5px; " >
-    <table cellspacing="15" cellpadding="0" >
-    <form  action="choess_blood_box_pdf.php" method="get">
-    <table  cellspacing="15" cellpadding="0" >
 
-<td>
-   Patinte ID:     <input type="number" id="Patinte" name="pat_id" /> </td>
-  <td>       <button type="submit"  aria-placeholder="Add" name="add_sess" class="btn btn-success" > SAVE </button>
-</td>
-
-</table>
-
-       <form  action="bookses_pdf_acch.php" method="get">
-
-<style>
-input[type='checkbox'] {
-    -webkit-appearance:none;
-    width:20px;
-    height:20px;
-    background:white;
-    border-radius:5px;
-    border:1px solid green;
-        cursor: pointer;
-
-}
-input[type='checkbox']:checked {
-    background: red;
-}
-</style>
-
-<div class="form-group" style="font-size:22px; font-family:Tahoma; "  ><!-- form-group Starts -->
-
-
-<lable style="color:red; font-size:22px; " > HAEMATOLOGY </lable>
-<br/>
-
-       <input type="checkbox" name="test[]" value="1" > CBC - </input>
-
-<select name="test[]" multiple >
-<option  value=""> CBC - فروع </option>
-<option name="test[]" value="101">HB </option>
-<option name="test[]" value="102">wbc </option>
-</select>
-
-        <input type="checkbox" name="test[]" value="2" > Platelats  </input>
-
-       <input type="checkbox" name="test[]" value="3" > ESR   </input>
-
-       <input type="checkbox" name="test[]" value="4" >  Malaria  </input>
-       
-       <input type="checkbox" name="test[]" value="7" > CT </input>
-
-       <input type="checkbox" name="test[]" value="8" > PT </input>
-
-    <!--   <select name="test[]">
-<option  value=""> PT - فروع </option>
-<option name="test[]" value="103">INR</option>
-</select>
--->
-       <input type="checkbox" name="test[]" value="9" > BT </input>
-
-       <input type="checkbox" name="test[]" value="10" > Reticulocyte </input>
-
-       <input type="checkbox" name="test[]" value="11" > Sickling test </input>
-       
-              <input type="checkbox" name="test[]" value="12" >  PTT </input>
-
-       <input type="checkbox" name="test[]" value="13" > D_Dimer  </input>
-
-<pre></pre>
-
-<lable style="color:red; font-size:22px; "> BIOCHEMEISTRY </lable>
-
-<pre></pre>
-
-       <input type="checkbox" name="test[]" value="14" > F.B.S   </input>
-
-       <input type="checkbox" name="test[]" value="15" >  R.B.S </input>
-
-       <input type="checkbox" name="test[]" value="16" > P.PBS  </input>
-
-       <input type="checkbox" name="test[]" value="17" > HBA 1C  </input>
-       
-       <input type="checkbox" name="test[]" value="18" > KFT </input>
-
- <select name="test[]" multiple >
-<option  value=""> KFT - فروع </option>
-<option name="test[]" value="104">Urea</option>
-<option name="test[]" value="105">Creatinine</option>
-</select>
-
-       <input type="checkbox" name="test[]" value="19" > LFT </input>
-
-        <select name="test[]" multiple >
-<option  value=""> LFT - فروع </option>
-<option name="test[]" value="106">S.Got</option>
-<option name="test[]" value="107">S.Gpt</option>
-<option name="test[]" value="108">Total Bilirubin</option>
-<option name="test[]" value="109">Dirict Bilirubin </option>
-</select>
-
-       <input type="checkbox" name="test[]" value="20" >ALK.Phospats </input>
-
-       <input type="checkbox" name="test[]" value="21" > Albumin</input>
-
-       <input type="checkbox" name="test[]" value="22" > Electrolytes  </input>
-
-               <select name="test[]" multiple >
-<option  value=""> Electrolytes - فروع </option>
-<option name="test[]" value="110"> Ca++ </option>
-<option name="test[]" value="111">K+</option>
-<option name="test[]" value="112">Na+</option>
-<option name="test[]" value="113"> Cl+ </option>
-<option name="test[]" value="114">Mg++</option>
-</select>
-       
-<pre></pre>
-
-
-       <input type="checkbox" name="test[]" value="23" > Cardiac Enzyme  </input>
-
-                      <select name="test[]" multiple >
-<option  value=""> Cardiac Enzyme - فروع </option>
-<option name="test[]" value="115"> C.K </option>
-<option name="test[]" value="116">CK-MB</option>
-<option name="test[]" value="117">L.D.H</option>
-</select>
-
-       <input type="checkbox" name="test[]" value="24" > Lipid  </input>
-
-                             <select name="test[]" multiple >
-<option  value=""> Lipid - فروع </option>
-<option name="test[]" value="118"> Cholesterol </option>
-<option name="test[]" value="119">Triglyceride</option>
-<option name="test[]" value="120">LDL</option>
-<option name="test[]" value="121">HDL</option>
-
-</select>
-
-       <input type="checkbox" name="test[]" value="25" >  Uricacid </input>
-
-       <input type="checkbox" name="test[]" value="39" >  T.Patinte </input>
-
-
-<pre></pre>
-
-<lable style="color:red; font-size:22px; "> SEROLOGY </lable>
-
-<pre></pre>
-
-       <input type="checkbox" name="test[]" value="26" > ASO  </input>
-
-       <input type="checkbox" name="test[]" value="27" > C.R.P  </input>
-       
-       <input type="checkbox" name="test[]" value="28" > RF </input>
-
-       <input type="checkbox" name="test[]" value="29" > Widal Test </input>
-
-       <input type="checkbox" name="test[]" value="30" > Brucella </input>
-
-       <input type="checkbox" name="test[]" value="31" > BLOOD Group </input>
-
-       <input type="checkbox" name="test[]" value="32" > TB  </input>
-       
-       
-       <input type="checkbox" name="test[]" value="33" > Viral Marker  </input>
-
-                             <select name="test[]" multiple >
-<option  value=""> Viral Marker - فروع </option>
-<option name="test[]" value="122"> HIV </option>
-<option name="test[]" value="123">HCV</option>
-<option name="test[]" value="124">HBS.AG</option>
-</select>
-       <input type="checkbox" name="test[]" value="36" >  VDRL   </input>
-
-       <input type="checkbox" name="test[]" value="37" > H.PYLORI RB </input>
-       
-       <input type="checkbox" name="test[]" value="38" > H.PYLORI AG </input>
-
-  
-<pre></pre>
-<lable style="color:red; font-size:22px; " > Drugs </lable>
-<pre></pre>
-
-     <input type="checkbox" name="test[]" value="40" > Ethanol  </input>
-
-       <input type="checkbox" name="test[]" value="41" > Diazepam  </input>
-       
-       <input type="checkbox" name="test[]" value="42" > Marijuana </input>
-
-       <input type="checkbox" name="test[]" value="43" > Tramedol </input>
-
-       <input type="checkbox" name="test[]" value="44" > Heroin </input>
-
-       <input type="checkbox" name="test[]" value="45" > Pethidine </input>
-
-       <input type="checkbox" name="test[]" value="46" > Cocaine  </input>
-
-              <input type="checkbox" name="test[]" value="47" > Amphetamine  </input>
-
-       <pre></pre>
-<lable style="color:red; font-size:22px; " > Harmones </lable>
-<pre></pre>
-
-<input type="checkbox" name="test[]" value="48" > T3  </input>
-
-       <input type="checkbox" name="test[]" value="49" > T4  </input>
-       
-       <input type="checkbox" name="test[]" value="50" > TSH </input>
-
-       <input type="checkbox" name="test[]" value="51" > Prolactin </input>
-
-       <input type="checkbox" name="test[]" value="52" > PSA Free </input>
-
-       <input type="checkbox" name="test[]" value="53" > PSA Total </input>
-
-       <input type="checkbox" name="test[]" value="54" > Vit-B12 </input>
-
-       <input type="checkbox" name="test[]" value="55" > Vit-D3 </input>
-
-       <input type="checkbox" name="test[]" value="56" > CA 153 </input>
-
-       <input type="checkbox" name="test[]" value="57" > CA 125 </input>
-
-    
-
-
-</form>
-                    </table>
-
+    <div class="boxlab" style="top:5px;">
+        <!-- نستخدم الفورم الرئيسية لإرسال الفحوصات المختارة -->
+        <!-- (الأفضل استعمال POST) -->
+        <form id="bloodtest-form" action="choess_blood_box_preview.php" method="post">
+            <table cellspacing="15" cellpadding="0">
+                <tr>
+                    <td>
+                        Patient ID:
+                        <input type="number" id="Patient" name="pat_id" required />
+                    </td>
+                    <td>
+                        <!-- زر إرسال لننتقل إلى صفحة المعاينة (preview) -->
+                        <button type="submit" name="preview" class="btn btn-success">عرض التكلفة</button>
+                    </td>
+                </tr>
+            </table>
+
+            <?php
+            // 1) جلب جميع الفئات من قاعدة البيانات
+            $sql_categories = "SELECT category_id, category_name FROM test_categories ORDER BY category_id ASC";
+            $res_categories = $conn->query($sql_categories);
+
+            if ($res_categories->num_rows > 0):
+                while ($cat_row = $res_categories->fetch_assoc()):
+                    $category_id   = $cat_row['category_id'];
+                    $category_name = $cat_row['category_name'];
+                    
+                    // عنوان الفئة
+                    echo "<h3 style='color:red; font-size:22px; margin-top:15px;'>{$category_name}</h3>";
+
+                    // 2) جلب الاختبارات الخاصة بهذه الفئة
+                    $sql_tests = "SELECT test_id, test_name, price 
+                                  FROM tests 
+                                  WHERE category_id = $category_id
+                                  ORDER BY test_id ASC";
+                    $res_tests = $conn->query($sql_tests);
+
+                    if ($res_tests->num_rows > 0):
+                        echo "<div style='margin-bottom:10px;'>";
+                        while ($test_row = $res_tests->fetch_assoc()):
+                            $test_id   = $test_row['test_id'];
+                            $test_name = $test_row['test_name'];
+                            $price     = $test_row['price'];
+
+                            // checkbox لكل اختبار
+                            echo "
+                                <label style='margin-right:15px;'>
+                                    <input type='checkbox' name='test[]' value='{$test_id}' />
+                                    {$test_name} ({$price})
+                                </label>
+                            ";
+                        endwhile;
+                        echo "</div>";
+                    else:
+                        echo "<p>لا توجد اختبارات ضمن هذه الفئة.</p>";
+                    endif;
+
+                endwhile;
+            else:
+                echo "<p>لا توجد فئات مُسجّلة بعد.</p>";
+            endif;
+            ?>
+        </form>
+    </div>
+</main>
+
+<!-- Modal Structure -->
+<div class="modal fade" id="confirmationModal" tabindex="-1" aria-labelledby="confirmationModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title text-success" id="confirmationModalLabel">تأكيد الفحوصات المختارة</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="إغلاق">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <!-- محتوى التأكيد سيتم تحميله هنا -->
+        <div id="confirmationContent">
+            <!-- سيتم ملؤه عبر AJAX -->
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">إغلاق</button>
+      </div>
+    </div>
+  </div>
 </div>
 
+<footer>
+    <!-- Footer content -->
+</footer>
 
-      </main>
+<!-- JavaScript to handle form submission and modal display -->
+<script>
+document.getElementById('bloodtest-form').addEventListener('submit', function(e) {
+    e.preventDefault(); // منع الإرسال التقليدي للنموذج
 
+    // إنشاء كائن FormData لجمع بيانات النموذج
+    var formData = new FormData(this);
 
-      <footer>
-
-        <!--
-<p id="demo"></p>
-
-            <script>
-            var d = new Date();
-            document.getElementById("demo").innerHTML = d;
-            </script>
-        -->    
-            
-
-      </footer>
-
-      
+    // إرسال البيانات عبر Fetch API باستخدام POST
+    fetch('choess_blood_box_preview.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        // تحميل البيانات في محتوى النموذج المنبثق
+        document.getElementById('confirmationContent').innerHTML = data;
+        // عرض النموذج المنبثق
+        $('#confirmationModal').modal('show');
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('حدث خطأ أثناء معالجة الطلب.');
+    });
+});
+</script>
 
 </body>
-
-
-
-
-
 </html>
-
