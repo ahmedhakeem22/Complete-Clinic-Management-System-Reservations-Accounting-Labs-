5b626ee762c429c03e00d21ea3615ac0d2d63b99
ahmedhakeem22 <58742511+ahmedhakeem22@users.noreply.github.com>
2024-12-25 15:55:55 +0300

    Commit for reception/includes/templates/header.php

--- reception/includes/templates/header.php
+++ reception/includes/templates/header.php
@@ -1,15 +1,33 @@
 <!DOCTYPE html>
-<html  lang="ar" dir="ltr" >  
+<html lang="ar" dir="ltr">  
 <head>
-            <title></title>
-            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-            <meta name="viewport" content="width=device-width , initial-scale=1, shrink-to-fit=no">
-            <link rel="icon" href="../images/icon1.png" type="image/png" sizes="16x16">
-            <link rel="stylesheet" type="text/css" href="../css/style.css">
-	<link rel="stylesheet" type="text/css" href="../css/style1.css">
-	 <link rel="stylesheet" href="../css/bootstrap.min.css">
-        <link rel="stylesheet" href="../css/font-awesome.min.css">
-            <title> Home </title>
-            <script src=""></script>
-        </head>
+    <meta charset="utf-8" />
+    <title>Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- أيقونة الموقع -->
+    <link rel="icon" href="../images/icon1.png" type="image/png" sizes="16x16">
+    
+    <!-- Bootstrap CSS من CDN -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    
+    <!-- ملفات CSS الخاصة بك -->
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/style1.css">
+</head>
 <body>
+
+    <!-- محتوى الصفحة -->
+
+    <!-- تضمين jQuery و Popper.js و Bootstrap JS من CDN -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
+    <!-- ملفات JavaScript الخاصة بك -->
+    <script src="../js/main.js"></script>
+</body>
+</html>
