de0dcd46ee5ca0363e2ca4f7dcd77dfafad6ee7d
ahmedhakeem22 <58742511+ahmedhakeem22@users.noreply.github.com>
2025-01-10 18:30:03 +0300

    `Added multiple CSS and JavaScript links to header.php`

--- blood/includes/templates/header.php
+++ blood/includes/templates/header.php
@@ -6,10 +6,19 @@
             <meta name="viewport" content="width=device-width , initial-scale=1, shrink-to-fit=no">
             <link rel="icon" href="../images/icon1.png" type="image/png" sizes="16x16">
             <link rel="stylesheet" type="text/css" href="css/style1.css">
+           
+
 	<link rel="stylesheet" type="text/css" href="css/style1.css">
 	 <link rel="stylesheet" href="../css/bootstrap.min.css">
         <link rel="stylesheet" href="../css/font-awesome.min.css">
+        
             <title> Home </title>
             <script src=""></script>
+             <!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Bootstrap JS Bundle (يشمل Popper) -->
         </head>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 <body>
